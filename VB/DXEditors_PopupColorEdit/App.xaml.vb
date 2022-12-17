29d166f79150ebfdaf1347fbeb6c31ff44f232e6
DevExpressExampleBot <fakeEmail>
2022-12-17 16:03:24 +0400

    Source auto update [skip ci]

---
+++ VB/DXEditors_PopupColorEdit/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXEditors_PopupColorEdit
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/DXEditors_PopupColorEdit/MainWindow.xaml.vb
+++ VB/DXEditors_PopupColorEdit/MainWindow.xaml.vb
@@ -1,33 +1,25 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
 Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Editors
 
 Namespace DXEditors_PopupColorEdit
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			' Removes the 'Standard Colors' palette.
-			popupColorEdit1.Palettes.Remove(popupColorEdit1.Palettes("Standard Colors"))
-			' Adds a custom gradient palette.
-			popupColorEdit1.Palettes.Add(CustomPalette.CreateGradientPalette("Apex Colors", PredefinedColorCollections.Apex))
-			' Adds a new palette with three custom RGB colors.
-			popupColorEdit1.Palettes.Add(New CustomPalette("Custom RGB Colors", New List(Of Color)() From {Color.FromRgb(150, 18, 30), Color.FromRgb(20, 40, 20), Color.FromRgb(88, 73, 29)}))
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            ' Removes the 'Standard Colors' palette.
+            Me.popupColorEdit1.Palettes.Remove(Me.popupColorEdit1.Palettes("Standard Colors"))
+            ' Adds a custom gradient palette.
+            Me.popupColorEdit1.Palettes.Add(ColorPalette.CreateGradientPalette("Apex Colors", PredefinedColorCollections.Apex))
+            ' Adds a new palette with three custom RGB colors.
+            Me.popupColorEdit1.Palettes.Add(New CustomPalette("Custom RGB Colors", New List(Of Color)() From {Color.FromRgb(150, 18, 30), Color.FromRgb(20, 40, 20), Color.FromRgb(88, 73, 29)}))
+        End Sub
+    End Class
 End Namespace
